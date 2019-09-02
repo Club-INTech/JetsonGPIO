bee027a70d26ad51e9cdc092ffe3d5f4233db2a4
jglrxavpok <jglrxavpok@gmail.com>
2019-09-02 19:26:52 +0200

    Test with modulation

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -10,11 +10,17 @@ public class Tests {
     @Test
     public void test_SetGEN0ToHigh() throws IOException {
         try(Pin pin = GPIO.open(Pins.GEN0, GPIO.Mode.OUTPUT)) {
-            pin.high();
-            try {
-                Thread.sleep(1000*60);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            for (int i = 0; i < 60; i++) {
+                if(i % 2 == 0) {
+                    pin.high();
+                } else {
+                    pin.low();
+                }
+                try {
+                    Thread.sleep(1000*60);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         } catch (IOException e) {
             throw e;
